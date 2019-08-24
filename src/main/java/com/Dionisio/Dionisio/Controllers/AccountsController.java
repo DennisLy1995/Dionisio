59066a850ad133c6cd067f8104d136df8fbdd721
Rafael Briceño <rbricenoa@ucenfotec.ac.cr>
2019-08-23 20:25:50 -0600

    Update AccountsController.java

--- src/main/java/com/Dionisio/Dionisio/Controllers/AccountsController.java
+++ src/main/java/com/Dionisio/Dionisio/Controllers/AccountsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,18 @@ public class AccountsController {
 	public List<ACCOUNT> getAllAccounts() {
 		List<ACCOUNT> accounts = repoA.findAll();
 		return accounts;
+	}
+	
+	@GetMapping(path= {"/GetAccount/{email}"})
+	public ACCOUNT getAccountByEmail(@PathVariable String email) {
+		List<ACCOUNT> accounts = repoA.findAll();
+		ACCOUNT account = null;
+		for(ACCOUNT bd : accounts) {
+			if(bd.getEMAIL().equals(email)) {
+				account = bd;
+			}
+		}
+		return account;
 	}
 	
 	@PostMapping("/registerAccount")
